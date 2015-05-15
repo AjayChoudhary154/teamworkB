6cfeda71ceac07fd0a89e301df0ff5b2eead252a
Manisha Rawat <manishar347@gmail.com>
2015-05-15 12:04:21 +0530

    login file update by manisha

---
+++ login222.java
@@ -0,0 +1,74 @@
+package log;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;  
+/**
+ * Servlet implementation class login
+ */
+
+public class login extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	private String pwd;
+	 public void init(ServletConfig servletConfig) throws ServletException{
+		    this.pwd = servletConfig.getInitParameter("upass");
+		    System.out.println("PWD:"+pwd);
+	 }
+	 
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public login() {
+        super();
+        
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	
+	response.setContentType("text/html");  
+    PrintWriter out = response.getWriter();  
+          
+    String name=request.getParameter("username");  
+    String pass=request.getParameter("userpass"); 
+    
+    String name1=request.getSession().getServletContext().getInitParameter("uname");
+    System.out.println(name);
+    System.out.println(pass);
+    System.out.println(name1);
+    if(name.equals(name1)&&pass.equals(this.pwd))
+    		{
+    	 System.out.println("correct");
+    	out.print("Welcome" +request.getParameter("username"));
+            }  
+    else
+            {  
+    	 System.out.println("incorrect");
+         out.print("Sorry username or password error");  
+         
+    }  
+          
+    out.close();  
+    }  
+   }
+
+
+ 
